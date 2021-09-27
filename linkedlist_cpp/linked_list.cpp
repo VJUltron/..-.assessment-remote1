15ea1412ff3fef586e07969bfa9f00f5d83daaca
John Doe <johndoe@example.com>
2021-09-27 19:57:10 +0000

    Initial Version
    Commit by HackerRank

---
+++ linkedlist_cpp/linked_list.cpp
@@ -0,0 +1,14 @@
+a = 10
+b = 40
+#perform logical AND
+c = a < 5 &&  b < 10
+#perform logical OR
+d = a < 5 || b < 4
+#perform logical NOT
+e = !(a < 5 && b < 10)
+#perform modulus of a and b
+f = a%b
+print 'The logical AND of a and b is: ',c
+print 'The logical OR of a and b is: ',d
+print 'The logical NOT of a and b is: ',e
+print 'The modulus of a and b is: ',f
